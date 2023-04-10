ebb78be2a2d50f68443199c38bd6a9711dd5208d
Vlad <blankvladp@gmail.com>
2023-04-10 22:12:00 +0300

    database connection

--- root/backend/server.js
+++ root/backend/server.js
@@ -1,5 +1,18 @@
 require("dotenv").config();
+const mongoose = require("mongoose");
 const app = require("./app");
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8000;
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.set("strictQuery", false);
+
+mongoose
+  .connect(DB)
+  .then(() => console.log("Database connected!"))
+  .catch((err) => console.log(err));
 
 app.listen(PORT, () => console.log("Listening to port 3000..."));
