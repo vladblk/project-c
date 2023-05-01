8b47f1cedb13847808f0e626140d411fcb1422ed
Vlad <blankvladp@gmail.com>
2023-05-01 16:00:09 +0300

    limit api calls

--- root/backend/app.js
+++ root/backend/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
 const destinationRouter = require('./routes/destinationRoutes');
 const userRouter = require('./routes/userRoutes');
 const AppError = require('./utils/appError');
@@ -6,6 +7,18 @@ const globalErrorHandler = require('./controllers/errorController');
 const app = express();
 
 app.use(express.json());
+
+// limit the api calls
+const apiLimiter = rateLimit({
+  windowMs: 60 * 60 * 1000, // 60 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  message: 'Too many requests registered. Please try again later!',
+});
+
+// Apply the rate limiting middleware to all requests
+app.use('/api', apiLimiter);
 
 // mount the destination router as a middleware
 app.use('/api/v1/destinations', destinationRouter);
