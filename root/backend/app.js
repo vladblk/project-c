6517f8da47ff1bacc0fafb79cf3074341964060a
Vlad <blankvladp@gmail.com>
2023-05-01 17:29:29 +0300

    setting security http headers, data sanitization and preventing paramter pollution

--- root/backend/app.js
+++ root/backend/app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 const destinationRouter = require('./routes/destinationRoutes');
 const userRouter = require('./routes/userRoutes');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const app = express();
 
-app.use(express.json());
+// security headers middleware
+app.use(helmet());
 
 // limit the api calls
 const apiLimiter = rateLimit({
@@ -19,6 +24,30 @@ const apiLimiter = rateLimit({
 
 // Apply the rate limiting middleware to all requests
 app.use('/api', apiLimiter);
+
+// parse data from body into req.body middleware
+app.use(express.json({ limit: '10kb' }));
+
+// data sanitization against query injections
+app.use(mongoSanitize());
+
+// data sanitization against xss
+app.use(xss());
+
+// prevent paramter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+      'country',
+    ],
+  })
+);
 
 // mount the destination router as a middleware
 app.use('/api/v1/destinations', destinationRouter);
